b6820369950e735ff7ad6ed766412f410fdfa0e7
ndukwon <ndukwon@gmail.com>
2017-03-31 18:04:15 +0900

    learning

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,8 +9,40 @@ Nam
 "hello~"
 '''
 
+print('\n##### printing arguments #####')
 print('{0} was {1} years old when he wrote this book'.format(name, age))
 print('{} was {} years old when he wrote this book'.format(name, age))
 print('was {} years old when he wrote this book'.format(name, age))
-# print('{} was {} years old when he wrote this book'.format(name))
+# print('{} was {} years old when he wrote this book'.format(name))     IndexError: tuple index out of range
 print('fullName={0}'.format(fullName))
+
+print('\n##### printing format #####')
+print('{0}'.format(1.0/3))
+print('{0:.3f}'.format(1.0/3))
+
+print('[{}]'.format("Hello!"))
+print('[{0:_^15}]'.format("Hello!"))
+print('[{0:^15}]'.format("Hello!"))
+print('[{0:#^15}]'.format("Hello!"))
+print('[{0:\'^15}]'.format("Hello!"))
+print('[{0:\\^15}]'.format("Hello!"))
+# print('{0:_15}'.format("Hello!"))     ValueError: Invalid format specifier
+
+print('{name} wrote {book}'.format(name='Duk', book='note'))
+
+print('\n##### printing escape characters #####')
+# r means raw.
+print('\' << This is a single quotation mark')
+print(r'\' << This is a single quotation mark')
+print('\\ << This is a back slash mark')
+print(r'\\ << This is a back slash mark')
+print('This \n is Enter')
+print(r'This \n is Enter')
+print('This \t is Tab')
+print(r'This \t is Tab')
+print('This \
+is attached')
+print(r'This \
+is attached')
+
+# p.42
