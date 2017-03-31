0b7f641267f3cc3e34b7ea38278a8a5f05e1656e
ndukwon <ndukwon@gmail.com>
2017-03-31 09:06:19 +0900

    learning

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,16 @@
 print("hello world!")
+
+age = 36
+name = 'Duk'
+fullName = '''
+Duk
+won
+Nam
+"hello~"
+'''
+
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('{} was {} years old when he wrote this book'.format(name, age))
+print('was {} years old when he wrote this book'.format(name, age))
+# print('{} was {} years old when he wrote this book'.format(name))
+print('fullName={0}'.format(fullName))
